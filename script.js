2f6827dd16af48e1ec88d53f3406d1b58961af43
Steven Adema <stevenadema@gmail.com>
2023-11-22 22:04:42 -0500

    add pause between combos

--- script.js
+++ script.js
@@ -32,66 +32,45 @@ function go() {
     let currentTimer = x;
     let intervalId;
     let cycleCount = 1;
-    var combos = [[ 1,1,2,3],[1,1,2,3],[1,1,2,3],[2,2,1,1,2,3]]
+    var combos = [[1,2,3],[1,1,2],[3,2,3,4],[1,1,2]]
     var audio = new Audio('fx/punch.mp3')
 
     startTimer();
     runTrainer(combos);
 
     function playAudio(url) {
-        new Audio(url).play();
+        // new Audio(url).play();
+        console.log("play audio");
       }
 
-    function runTrainer(combos){
+    function applyTransform(elementId, scale, rotation, translate_x, translate_y) {
+        $(`#${elementId}`).css({ 
+            transition: 'transform 0.2s ease-in-out',
+            transform: `scale(${scale}) rotate(${rotation}deg) translate(${translate_x}%, ${translate_y}%)`
+        });
+        setTimeout(function() {
+            $(`#${elementId}`).css({
+                transform: 'scale(1) rotate(0deg) translate(-50%, -50%)'
+            });
+        }, 200);
+        playAudio('fx/punch1.mp3');
+    }
+
+    function runTrainer(combos){ 
         let delay = 0;
         for (let i = 0; i < combos.length; i++) {
             const combo = combos[i];
             for (let j = 0; j < combo.length; j++) {
                 const punch = combo[j];
                 setTimeout(function(){
-                    if (punch == 1) {
-                        console.log("jab")
-                        $('#pad-left').css({ 
-                            transition: 'transform 0.2s ease-in-out',
-                            transform: 'scale(1.4) rotate(10deg) translate(-40%, -40%)'
-                        });
-                        playAudio('fx/punch1.mp3');
-                        setTimeout(function() {
-                            $('#pad-left').css({
-                                transform: 'scale(1) rotate(0deg) translate(-50%, -50%)'
-                            });
-                        }, 200);
-                    }
-                    if (punch == 2) {
-                        console.log("cross")
-                        $('#pad-right').css({ 
-                            transition: 'transform 0.2s ease-in-out',
-                            transform: 'scale(1.4) rotate(-10deg) translate(-35%, -50%)'
-                        });
-                        playAudio('fx/punch1.mp3');
-                        setTimeout(function() {
-                            $('#pad-right').css({
-                                transform: 'scale(1) rotate(0deg) translate(-50%, -50%)'
-                            });
-                        }, 200);
-                    }
-                    if (punch == 3) {
-                        console.log("hook");
-                        $('#pad-left').css({ 
-                            transition: 'transform 0.2s ease-in-out',
-                            transform: 'scale(1.2) rotate(-90deg) translate(45%, -30%)'
-                        });
-                        playAudio('fx/punch1.mp3')
-                        setTimeout(function() {
-                            $('#pad-left ').css({
-                                transform: 'scale(1) rotate(0deg) translate(-50%, -50%)'
-                            });
-                        }, 200);
-                    }
+                    if (punch == 1) {applyTransform('pad-left', 1.4, 10, -40, -40);}
+                    if (punch == 2) {applyTransform('pad-right', 1.4, -10, -35, -50);}
+                    if (punch == 3) {applyTransform('pad-left', 1.2, -90, 50, -30);}
+                    if (punch == 4) {applyTransform('pad-right', 1.4, -10);}
                 }, delay);
-
-                delay += 300; // 1000 ms for animation + 1000 ms pause
+                delay += 400;
             }
+            delay += 2000; // Add a pause of 2 seconds after each combo
         }
     }
 
@@ -154,26 +133,6 @@ $(document).ready(function () {
 });
 
 var audio = document.getElementById("audio");
-audio.volume = 0.2;
+audio.volume = 0.5;
 
-var combos = [[1,1,2,3],[1,3],[1,1],[2,3,2]]
-function runTrainer(combos, ){
-    for (let i = 0; i < combos.length; i++) {
-        const combo = combos[i];
-        for (let j = 0; j < combo.length; j++) {
-            const punch = combo[j];
-            if (punch == 1) {
-                document.getElementById('jab').style.backgroundColor = "red";
-                setTimeout(function(){ document.getElementById('pad-right').style.backgroundColor = "white"; }, 500);
-            }
-            if (punch == 2) {
-                document.getElementById('cross').style.backgroundColor = "red";
-                setTimeout(function(){ document.getElementById('pad-right').style.backgroundColor = "white"; }, 500);
-            }
-            if (punch == 3) {
-                document.getElementById('hook').style.backgroundColor = "red";
-                setTimeout(function(){ document.getElementById('pad-right').style.backgroundColor = "white"; }, 500);
-            }
-        }
-    }
-}
+go();
